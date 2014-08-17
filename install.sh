3da005b244df7936a055b18c4326d55593b20a10
pi <pi@raspi>
2014-08-17 09:13:43 +0000

    Working verion. This one is tried on the nagios3 server

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ sudo mkdir /var/log/wifi/ 2> /dev/null
 sudo chown -R nagios:adm /var/log/wifi/
 sudo cp -i examples/empty_ap_database.db /var/log/wifi/access_points.db
 sudo chmod -R 770 /var/log/wifi
-sudo cp config/* /etc/nagios-plugins/config/
+sudo mkdir /etc/nagios-wifi/ 2> /dev/null
+sudo cp config/wifi_scan.cfg /etc/nagios-wifi/
+sudo cp config/check_wifi_spectrum.cfg /etc/nagios-plugins/config/
 echo "[OK] installation done...."
 echo "... now fill database by running /usr/lib/nagios/plugins/check_wifi_spectrum -a as a user from the 'adm' group."
