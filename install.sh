2a8e94e0dc6ac345858c06879272cc2ded8c6c97
pi <pi@raspi>
2014-08-17 08:52:20 +0000

    Added some files this is beta 0.8

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo cp scripts/check_wifi_spectrum /usr/lib/nagios/plugins/check_wifi_spectrum
+sudo chmod +x /usr/lib/nagios/plugins/check_wifi_spectrum
+sudo mkdir /var/log/wifi/ 2> /dev/null
+sudo chown -R nagios:adm /var/log/wifi/
+sudo cp -i examples/empty_ap_database.db /var/log/wifi/access_points.db
+sudo chmod -R 770 /var/log/wifi
+sudo cp config/* /etc/nagios-plugins/config/
+echo "[OK] installation done...."
+echo "... now fill database by running /usr/lib/nagios/plugins/check_wifi_spectrum -a as a user from the 'adm' group."
