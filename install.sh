35cfdc8ba932a259da37b393eee3dc260f3b5217
pi <pi@raspi>
2014-09-28 15:41:24 +0200

    New version of the nagios wifi scan tool

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
-sudo cp scripts/check_wifi_spectrum /usr/lib/nagios/plugins/check_wifi_spectrum
+# copy plugin into plugin folder of nagios
+sudo cp -i scripts/check_wifi_spectrum /usr/lib/nagios/plugins/check_wifi_spectrum
 sudo chmod +x /usr/lib/nagios/plugins/check_wifi_spectrum
-sudo mkdir /var/log/wifi/ 2> /dev/null
-sudo chown -R nagios:adm /var/log/wifi/
-sudo cp -i examples/empty_ap_database.db /var/log/wifi/access_points.db
-sudo chmod -R 770 /var/log/wifi
-sudo mkdir /etc/nagios-wifi/ 2> /dev/null
-sudo cp config/wifi_scan.cfg /etc/nagios-wifi/
-sudo cp config/check_wifi_spectrum.cfg /etc/nagios-plugins/config/
+
+sudo mkdir /var/lib/nagios-wifi/ 2> /dev/null
+sudo chown -R nagios:adm /var/lib/nagios-wifi/
+sudo cp -i examples/empty_ap_database.db /var/lib/nagios-wifi/access_points.db
+sudo chmod -R 770 /var/lib/nagios-wifi
 echo "[OK] installation done...."
-echo "... now fill database by running /usr/lib/nagios/plugins/check_wifi_spectrum -a as a user from the 'adm' group."
+echo "... now fill database by running /usr/lib/nagios/plugins/check_wifi_spectrum -a as an user from the 'adm' group."
